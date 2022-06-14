c1e6dd4bfb4d65ac70c2ce16ca726625b1e1004b
kelvinfloresta <kelvinf.contato@gmail.com>
2022-06-14 18:06:23 -0300

    feat: add InteractiveApp

---
+++ src/InteractiveApp.ts
@@ -0,0 +1,25 @@
+import readline from 'readline'
+import { App } from './App'
+import { Console } from './Logger/Console'
+
+export class InteractiveApp {
+  constructor(private readonly app: App) {}
+
+  start() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+
+    rl.on('line', command => {
+      this.app.execute(command)
+    })
+
+    rl.on('close', () => {
+      console.log('\nSee you soon!')
+      process.exit(0)
+    })
+
+    console.log('Waiting for a command... \n')
+  }
+}
